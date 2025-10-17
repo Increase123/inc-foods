ddbdf0daa4730cdec558dd4e29793ba0bb966640
Increase <increaseakinwole46@gmail.com>
2025-10-17 15:03:41 +0100

    Added the footer section

---
+++ src/components/Footer.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import {
+  FaFacebookSquare,
+  FaInstagram,
+  FaTwitterSquare,
+  FaTiktok,
+} from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <div className="max-w-[1240px] mx-auto py-16 px-4 grid lg:grid-cols-3 gap-8">
+      <div>
+        <h1 className="text-2xl sm:text-3xl lg:text-4xl">
+          Inc <span className="font-bold">Foods</span>
+        </h1>
+        <p className="py-4">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id odit
+          ullam iste repellat consequatur libero reiciendis, blanditiis
+          accusantium.
+        </p>
+
+        <div className="flex justify-between md:w-[75%] my-6">
+          <FaFacebookSquare size={30} />
+          <FaInstagram size={30} />
+          <FaTwitterSquare size={30} />
+          <FaTiktok size={30} />
+        </div>
+      </div>
+
+      <div className="lg:col-span-2 flex justify-between mt-6">
+        <div>
+          <h6 className="font-medium">Support</h6>
+          <ul>
+            <li className="py-2 text-sm">Pricing</li>
+            <li className="py-2 text-sm">Documentation</li>
+            <li className="py-2 text-sm">Guides</li>
+            <li className="py-2 text-sm">API Status</li>
+          </ul>
+        </div>
+
+        <div>
+          <h6 className="font-medium">Company</h6>
+          <ul>
+            <li className="py-2 text-sm">About</li>
+            <li className="py-2 text-sm">Blog</li>
+            <li className="py-2 text-sm">Jobs</li>
+            <li className="py-2 text-sm">Press</li>
+            <li className="py-2 text-sm">Careers</li>
+          </ul>
+        </div>
+
+        <div>
+          <h6 className="font-medium">Legal</h6>
+          <ul>
+            <li className="py-2 text-sm">Claim</li>
+            <li className="py-2 text-sm">Policy</li>
+            <li className="py-2 text-sm">Terms</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
